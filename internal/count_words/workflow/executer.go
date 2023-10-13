937f527af69439ca62f34c664989c6a94c43ecda
Julio Souza <julio@papercup.com>
2023-10-13 14:35:45 +0100

    fix: code rabbit fixes

--- internal/count_words/workflow/executer.go
+++ internal/count_words/workflow/executer.go
@@ -8,21 +8,51 @@ import (
 )
 
 func Execute(c client.Client, config *WorkflowConfig, args ...interface{}) {
-	options := client.StartWorkflowOptions{
-		ID:               config.ID,
-		TaskQueue:        config.TaskQueue,
-		SearchAttributes: config.Metadata,
-	}
+
+	options := buildOptions(config)
 
 	we, err := c.ExecuteWorkflow(context.Background(), options, config.Workflow, args...)
 	if err != nil {
 		log.Fatalln("Unable to execute workflow", err)
 	}
-	log.Println("Started workflow", "WorkflowID", we.GetID(), "RunID", we.GetRunID(), "CorrelationId", options.SearchAttributes["correlationId"])
+
+	logWorkflowStart(we, options)
 
 	var result map[string]int
 	err = we.Get(context.Background(), &result)
 	if err != nil {
 		log.Fatalln("Unable get workflow result", err)
 	}
+}
+
+func buildOptions(config *WorkflowConfig) client.StartWorkflowOptions {
+
+	options := client.StartWorkflowOptions{}
+
+	if config != nil {
+		options.ID = config.ID
+		options.TaskQueue = config.TaskQueue
+
+		if config.Metadata != nil {
+			options.SearchAttributes = config.Metadata
+		}
+	}
+	return options
+}
+
+func logWorkflowStart(we client.WorkflowRun, options client.StartWorkflowOptions) {
+	workflowId := we.GetID()
+	runId := we.GetRunID()
+
+	var correlationId interface{}
+	if options.SearchAttributes != nil {
+		var ok bool
+		if correlationId, ok = options.SearchAttributes["correlationId"]; !ok {
+			correlationId = "none"
+		}
+	} else {
+		correlationId = "none"
+	}
+
+	log.Println("Started workflow", "WorkflowID", workflowId, "RunID", runId, "CorrelationId", correlationId)
 }
