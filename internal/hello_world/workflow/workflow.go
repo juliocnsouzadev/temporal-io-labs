8e646e381f61ebb520b3b06b0d4a69c246b1b471
Julio Souza <julio@papercup.com>
2023-10-17 18:03:32 +0100

    chore: change log level to debug

--- internal/hello_world/workflow/workflow.go
+++ internal/hello_world/workflow/workflow.go
@@ -8,6 +8,6 @@ import (
 
 func HelloWorldWorkflow(ctx workflow.Context, params string) (string, error) {
 	message := fmt.Sprintf("Hello World! => %s", params)
-	workflow.GetLogger(ctx).Info(message)
+	workflow.GetLogger(ctx).Debug(message)
 	return message, nil
 }
